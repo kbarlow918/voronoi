98168564ab1ea39ecb8170b60837ece166920b28
Joe Wolter <wolter.joe@gmail.com>
2012-02-08 05:42:23 -0500

    Fixed the out of bounds point generation error

--- Voronoi/RndPointSet.cpp
+++ Voronoi/RndPointSet.cpp
@@ -15,33 +15,55 @@ void RndPointSet::DrawPoints( const CRhinoCommandContext& context, int numPoints
  
   // Pick a surface to evaluate
   CRhinoGetObject go;
-  go.SetCommandPrompt( L"Select surface to evaluate" );
+  go.SetCommandPrompt( L"Select surface to evaluate " );
   go.SetGeometryFilter( CRhinoGetObject::surface_object);
   go.GetObjects( 1, 1 );
-
  
   // Get the surface geometry
   const CRhinoObjRef& ref = go.Object(0);
+
+  if(ref == NULL)
+  {
+	RhinoApp().Print(L"reference initialization error");
+	return ;
+  }
+
   const ON_Surface* obj = ref.Surface();
 
+  if(obj == NULL)
+  {
+	RhinoApp().Print(L"object initialization error");
+	return ;
+  }
+	
   double u1, u2, v1, v2;
-  
-  if(obj->GetDomain(0, &u1, &u2) && obj->GetDomain(0, &v1, &v2))
+
+  if(obj->GetDomain(0, &u1, &u2) && obj->GetDomain(1, &v1, &v2))
   {
 	  int i;
 	  for(i = 0; i < numPoints; i++)
 	  {
 		  ON_3dPoint p0 = obj->PointAt( fRand(u1, u2), fRand(v1, v2));
-		  double u, v;
-		  obj->GetClosestPoint(p0, &u, &v);
+		  //context.m_doc.AddPointObject(p0); 
+		  //RhinoApp().Print(L"p0.x = %f\n",p0.x);
+		  //RhinoApp().Print(L"p0.y = %f\n",p0.y);
+		  //RhinoApp().Print(L"p0.z = %f\n",p0.z);
+		  double u, v = 0.0;						//the following might be redundant
+		  obj->GetClosestPoint(p0, &u, &v);			
+		  //RhinoApp().Print(L"p0.u = %f\n",u);
+		  //RhinoApp().Print(L"p0.v = %f\n",v);
 		  ON_3dPoint p1 = obj->PointAt( u, v);
-
+		  
 		  context.m_doc.AddPointObject(p1);
 	  }
 	  context.m_doc.Redraw();
   }
+  else
+  {
+	RhinoApp().Print(L"object domain error");
+	return ;
+  }
 
- //this is drawing things outside the bounding box for some reason
 }
 
 void RndPointSet::Test( const CRhinoCommandContext& context, double a, double b, double c, double d )
